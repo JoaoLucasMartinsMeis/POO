d99966456f225a124b5bdeea802a584aef94320b
JOÃO LUCAS MARTINS MEIS <joao_meis@estudante.sesisenai.org.br>
2024-09-20 19:10:43 -0300

    Initial commit

---
+++ src/Exerc03/Main.java
@@ -0,0 +1,12 @@
+package Exerc03;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculadora calculo = new Calculadora();
+
+        calculo.informarNumero();
+
+        calculo.getNumero();
+    }
+}
